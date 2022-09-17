1976230a89441c21f0e2cb0e41e336f601082456
RyanBosch <112971119+RyanBosch@users.noreply.github.com>
2022-09-17 14:36:56 -0400

    Update main.java

--- main.java
+++ main.java
@@ -12,39 +12,25 @@ import java.util.concurrent.TimeUnit;
 import javax.swing.*;
 import net.proteanit.sql.DbUtils;
  
-public class main {
-     
-    public static class ex{
-        public static int days=0;
-            }
- 
-    public static void main(String[] args) {
-         
-        login();
-        //create();ma
-    }
- 
-    public static ArrayList<Book> readBooksArrayList() {
-		ArrayList<Book> books = new ArrayList<Book>();
-		try {
-			File bookFile = new File("books.csv");
-		    Scanner myReader = new Scanner(bookFile);
-		    myReader.nextLine();
-		    while (myReader.hasNextLine()) {		        
-		    	String[] bookInfo = myReader.nextLine().split(",", 23);
-		    	books.add(new Book(Integer.parseInt(bookInfo[0]), Integer.parseInt(bookInfo[1]), Integer.parseInt(bookInfo[2]), Integer.parseInt(bookInfo[3]), Integer.parseInt(bookInfo[4]), bookInfo[5], bookInfo[6], Float.parseFloat(bookInfo[7]), bookInfo[8], bookInfo[9], bookInfo[10], Float.parseFloat(bookInfo[11]), Integer.parseInt(bookInfo[12]), Integer.parseInt(bookInfo[13]), Integer.parseInt(bookInfo[14]), Integer.parseInt(bookInfo[15]), Integer.parseInt(bookInfo[16]), Integer.parseInt(bookInfo[17]), Integer.parseInt(bookInfo[18]), Integer.parseInt(bookInfo[19]), bookInfo[20], bookInfo[21], bookInfo[22]));
-		    }
-		    myReader.close();
-		} catch (FileNotFoundException e) {
-		   	System.out.println("Could not find the file.");
-	    	e.printStackTrace();
-	    }
+public class main { 
+   public static void main(String[] args) {
+		List<Book> books;
+		books = readBooks();
 		
-		return books;
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Libpp window = new Libpp(books);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
 	}
 	
-	public static LinkedList<Book> readBooksLinkedList() {
-		LinkedList<Book> books = new LinkedList<Book>();
+	public static List<Book> readBooks() {
+		List<Book> books = new ArrayList<Book>();
+		//List<Book> books = new LinkedList<Book>();
 		try {
 			File bookFile = new File("books.csv");
 		    Scanner myReader = new Scanner(bookFile);
